9b4da8cda04163259a97f69c665197ff2acec56e
charlietw <cwren92@gmail.com>
2016-12-13 22:33:56 +0000

    Add files via upload

---
+++ tournament/tournament.sql
@@ -0,0 +1,33 @@
+-- Table definitions for the tournament project.
+--
+-- Put your SQL 'create table' statements in this file; also 'create view'
+-- statements if you choose to use it.
+--
+-- You can write comments in this file by starting them with two dashes, like
+-- these lines here.
+
+--To store the players.
+create table players(
+    name text,
+    id serial,
+    PRIMARY KEY(id)
+);
+
+--To store the outcome of the matches played.
+create table matches(
+    winner integer references players(id),
+    loser integer references players(id),
+    id serial,
+    PRIMARY KEY(id)
+);
+
+--Selects the players, ordered by the number of matches they have won (also includes total matches played).
+create view standings AS
+        SELECT players.id AS id, players.name AS name, count(matches.winner) AS wins,
+        --creates a column called 'total', scanning both the 'winner' and 'loser' column of matches (with an OR)
+        (SELECT count(*) AS total FROM matches WHERE loser = players.id OR winner = players.id)
+        --Selects ALL rows from players (left) PLUS any rows from matches (right) where the id = the player id
+        FROM players LEFT JOIN matches
+        on matches.winner = players.id
+        GROUP BY players.id
+        ORDER BY wins DESC;
